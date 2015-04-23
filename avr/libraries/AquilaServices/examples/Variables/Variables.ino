b2e42d5ee9105c2f58189494c45090bdbabe6145
Rodrigo Mendez <rmendez@makerlab.mx>
2015-04-23 13:07:04 -0500

    updated new Services examples

--- avr/libraries/AquilaServices/examples/Variables/Variables.ino
+++ avr/libraries/AquilaServices/examples/Variables/Variables.ino
@@ -3,6 +3,32 @@
 #include <AquilaProtocol.h>
 #include <AquilaServices.h>
 
+/*
+	Aquila Services Variables example
+
+	In this example we explore how we can expose
+	int or float variables as services.
+
+	This way, we can treat global variables as we
+	normally do, while being able to consult their 
+	current value at any time.
+
+	We have 2 variables, called count1 and count2.
+	In the setup, we expose them as services with
+	the function Services.variable("serviceName", &variableName).
+	Please note the 'ampersand' (&), this is needed as we are
+	passing the pointer of the variable.
+
+	Then we can normally access and change the variables in the
+	loop.
+
+	To consult the current value of the variable, we can make a 
+	request to the Hub API and get the value as a JSON object,
+	for example, we could get: {"val":123}.
+
+	For more information, see: http://docs.aquila2.apiary.io/
+ */
+
 int count1;
 float count2;
 
